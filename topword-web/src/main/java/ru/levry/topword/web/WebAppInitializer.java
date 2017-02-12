64ef7ac05919eaba25c3f9b3346335bc5a40ce74
levry <lriskov@gmail.com>
2017-02-12 22:13:23 +0500

    Adapt to tomcat 7

--- topword-web/src/main/java/ru/levry/topword/web/WebAppInitializer.java
+++ topword-web/src/main/java/ru/levry/topword/web/WebAppInitializer.java
@@ -1,5 +1,7 @@
 package ru.levry.topword.web;
 
+import javax.servlet.Filter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 import ru.levry.topword.web.config.TopwordConfig;
 import ru.levry.topword.web.config.WebMvcConfig;
@@ -8,6 +10,20 @@ import ru.levry.topword.web.config.WebMvcConfig;
  * @author levry
  */
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter[]{
+                getCharacterEncodingFilter()
+        };
+    }
+
+    private CharacterEncodingFilter getCharacterEncodingFilter() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
